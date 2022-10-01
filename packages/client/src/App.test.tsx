c9808651b4af01f784886946e1b38831b92054f5
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-10-01 19:38:48 +0300

    feat: example test

---
+++ packages/client/src/App.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
+// @ts-ignore
+global.fetch = jest.fn(() =>
+  Promise.resolve({ json: () => Promise.resolve('hey') })
+);
+
+test('Example test', async () => {
+  render(<App />);
+  expect("Example").toBe("Example");
+});
