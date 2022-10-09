1856e622814c1661acf5e50e75fee9adc7ee398f
Aleksandr <loskutov@aleksandrl.ru>
2022-10-09 17:58:39 +0300

    add signUp logic

--- packages/client/src/pages/signUp/index.tsx
+++ packages/client/src/pages/signUp/index.tsx
@@ -1,84 +1,87 @@
 import React, { useCallback } from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Alert } from 'antd';
 import { useNavigate } from 'react-router';
 import { Rule } from 'antd/lib/form';
 import 'antd/dist/antd.css';
 import './style.css';
-import { withNaming } from '@bem-react/classname';
 import signUpRules from './validator';
+import signUp from './services/signup-service';
+import createCn from '../../utils/create-cn';
 
-interface FormData {
+type FormData = {
   email: string;
   login: string;
-  name?: boolean;
-  surname?: string;
+  first_name: string;
+  second_name: string;
   phone: string;
   password: string;
   passwordRepeat: string;
-}
-
-// todo заменить на from utils когда будет готов
-const cn = (block: string, element?: string, modifier?: any) => {
-  const preset = { e: '__', m: '_', v: '_' };
-  const baseCn = withNaming(preset);
-
-  return baseCn(block, element)(modifier);
 };
+
+const cn = createCn('sign-up');
 
 function SignUpPage(): JSX.Element {
   const navigate = useNavigate();
+  const [formAlert, setFormAlert] = React.useState<string | null>(null);
   const onLoginClick = useCallback((): void => {
     navigate('/sign-in');
   }, []);
-  const handleSubmit = (values: FormData) => {
-    console.log('Submit:', values);
-  };
+  const handleSubmit = useCallback(async (data: FormData) => {
+    const res = await signUp(data);
+    if (res.error) {
+      setFormAlert(res.error);
+      return;
+    }
+    if (res.data?.id) {
+      navigate('/');
+    }
+  }, []);
 
   return (
-    <div className={cn('sign-up')}>
+    <div className={cn()}>
       <Form
-        className={cn('sign-up', 'form')}
+        className={cn('form')}
         layout="vertical"
         initialValues={{ remember: true }}
         onFinish={handleSubmit}>
-        <h1 className={cn('sign-up', 'form-title')}>Регистрация</h1>
+        <h1 className={cn('form-title')}>Регистрация</h1>
         <Form.Item
-          className={cn('sign-up', 'form-item')}
+          className={cn('form-item')}
           label="Почта"
           name="email"
           rules={signUpRules.email as Rule[]}>
           <Input />
         </Form.Item>
         <Form.Item
-          className={cn('sign-up', 'form-item')}
+          className={cn('form-item')}
           label="Логин"
           name="login"
           rules={signUpRules.login}>
           <Input />
         </Form.Item>
         <Form.Item
-          className={cn('sign-up', 'form-item')}
+          className={cn('form-item')}
           label="Имя"
           name="first_name"
-          rules={signUpRules.firsName}>
+          rules={signUpRules.firstName}>
           <Input />
         </Form.Item>
         <Form.Item
-          className={cn('sign-up', 'form-item')}
+          className={cn('form-item')}
           label="Фамилия"
           name="second_name"
-          rules={signUpRules.firsName}>
+          rules={signUpRules.secondName}>
           <Input />
         </Form.Item>
         <Form.Item
-          className={cn('sign-up', 'form-item')}
+          className={cn('form-item')}
           label="Телефон"
           name="phone"
           rules={signUpRules.phone}>
           <Input />
         </Form.Item>
         <Form.Item
-          className={cn('sign-up', 'form-item')}
+          className={cn('form-item')}
           name="password"
           label="Пароль"
           rules={signUpRules.password}
@@ -87,7 +90,7 @@ function SignUpPage(): JSX.Element {
         </Form.Item>
 
         <Form.Item
-          className={cn('sign-up', 'form-item')}
+          className={cn('form-item')}
           name="passwordRepeat"
           label="Пароль (еще раз)"
           dependencies={['password']}
@@ -108,13 +111,20 @@ function SignUpPage(): JSX.Element {
           ]}>
           <Input.Password />
         </Form.Item>
-        <Form.Item className={cn('sign-up', 'form-item')}>
+        {formAlert && (
+          <>
+            <br />
+            <Alert message={formAlert} type="error" />
+            <br />
+          </>
+        )}
+        <Form.Item className={cn('form-item')}>
           <Button type="primary" htmlType="submit" block>
             Зарегистрироваться
           </Button>
         </Form.Item>
 
-        <Form.Item className={cn('sign-up', 'form-item')}>
+        <Form.Item className={cn('form-item')}>
           <Button type="link" htmlType="button" block onClick={onLoginClick}>
             Войти
           </Button>
