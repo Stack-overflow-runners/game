5737510c1ec6bd531f822df54ecbf3db28549127
wildnefalem5 <wilderedin5@gmail.com>
2022-10-16 18:27:11 +0300

    add api for work with profile

--- packages/client/src/api/auth.ts
+++ packages/client/src/api/auth.ts
@@ -4,30 +4,20 @@ import { UserDTO } from '../types/user';
 import { ApiResponse } from '../types/api';
 
 class AuthAPI extends BaseAPI {
-  private static instance: AuthAPI;
-
-  private constructor() {
+  constructor() {
     super('/auth');
   }
 
-  public static getInstance(): AuthAPI {
-    if (!AuthAPI.instance) {
-      AuthAPI.instance = new AuthAPI();
-    }
-
-    return AuthAPI.instance;
-  }
-
-  signIn(payload: SignInDTO): ApiResponse<UserDTO> {
+  signIn(body: SignInDTO): ApiResponse<UserDTO> {
     return this.httpService.post('/signin', {
-      body: JSON.stringify(payload),
+      body,
       headers: { 'Content-Type': 'application/json' },
     });
   }
 
-  signUp(payload: SignUpDTO): ApiResponse<UserDTO> {
+  signUp(body: SignUpDTO): ApiResponse<UserDTO> {
     return this.httpService.post('/signup', {
-      body: JSON.stringify(payload),
+      body,
       headers: { 'Content-Type': 'application/json' },
     });
   }
@@ -43,6 +33,6 @@ class AuthAPI extends BaseAPI {
   }
 }
 
-const authAPI = AuthAPI.getInstance();
+const authAPI = new AuthAPI();
 
 export default authAPI;
