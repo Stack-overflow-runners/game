5ac743c8ebdb601eb277132298fb485caab20d70
Aleksandr <loskutov@aleksandrl.ru>
2022-10-22 23:44:20 +0300

    Add auth hook

---
+++ packages/client/src/hooks/auth.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useContext, createContext } from 'react';
+import { useAppDispatch, useAppSelector } from './store';
+import { SignInDTO, SignUpDTO } from '../types/auth';
+import {
+  fetchUser,
+  signIn as login,
+  signOut as logout,
+  signUp as register,
+} from '../store/action-creators/auth';
+import { UserDTO } from '../types/user';
+import { Nullable } from '../types/common';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+type AuthContextProps = {
+  user: Nullable<UserDTO>;
+  signUp: (credentials: SignUpDTO) => any;
+  signIn: (credentials: SignInDTO) => any;
+  signOut: () => any;
+  error: string | null;
+  isLoading: boolean;
+  isLoggedIn: boolean;
+};
+
+const authContext = createContext<AuthContextProps>({} as AuthContextProps);
+export const useAuth = () => useContext(authContext);
+
+function useAuthProvider() {
+  const dispatch = useAppDispatch();
+  const { user } = useAppSelector(state => state.user);
+  const { error } = useAppSelector(state => state.user);
+  const { isLoading } = useAppSelector(state => state.user);
+  const { isLoggedIn } = useAppSelector(state => state.user);
+
+  const signUp = (credentials: SignUpDTO): void => {
+    dispatch(register(credentials));
+  };
+
+  const signIn = (credentials: SignInDTO): void => {
+    dispatch(login(credentials));
+  };
+
+  const signOut = (): void => {
+    dispatch(logout());
+  };
+
+  const getUser = (): void => {
+    if (!user) {
+      dispatch(fetchUser());
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      getUser();
+    }
+  }, []);
+
+  return {
+    user,
+    signUp,
+    signIn,
+    signOut,
+    error,
+    isLoading,
+    isLoggedIn,
+  };
+}
+
+export function AuthProvider({ children }: Props) {
+  const auth = useAuthProvider();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
