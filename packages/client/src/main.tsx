ca6e5aae3bc305568a4ac3b3f6d6a2d48e3f1bde
Aleksandr <loskutov@aleksandrl.ru>
2022-10-26 17:56:55 +0300

    Add provider

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,15 +4,22 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
 import './styles/index.css';
+import { AuthProvider } from './hooks/auth';
 
 function startServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker.register("/sw.js").then(registration => {
-        console.log("ServiceWorker registration successful with scope: ", registration.scope);
-      }).catch((error: string) => {
-        console.log("ServiceWorker registration failed: ", error);
-      });
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(registration => {
+          console.log(
+            'ServiceWorker registration successful with scope: ',
+            registration.scope
+          );
+        })
+        .catch((error: string) => {
+          console.log('ServiceWorker registration failed: ', error);
+        });
     });
   }
 }
@@ -22,7 +29,9 @@ startServiceWorker();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Provider>
   </React.StrictMode>
 );
