42aefc1f7483fd5b9fa027e7f782d4b936b41bea
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2022-10-30 13:50:43 +0300

    feat: LeaderAPI

--- packages/client/src/utils/consts.ts
+++ packages/client/src/utils/consts.ts
@@ -1,3 +1,6 @@
 const API_URL = 'https://ya-praktikum.tech/api/v2';
+// Team name. Used to make unique leaderboard for each project. Yandex API
+const TEAM = 'stackoverflowRunners';
+const RATING_FIELD = 'score';
 
-export default { API_URL };
+export default { API_URL, TEAM, RATING_FIELD };
