67d173b59bf4f0592e7e9cec40db4111e328a10c
Aleksandr <loskutov@aleksandrl.ru>
2022-11-23 13:31:20 +0300

    Update HOC for OAuth

--- packages/client/src/hoc/withAuth.tsx
+++ packages/client/src/hoc/withAuth.tsx
@@ -1,18 +1,24 @@
-import { useNavigate } from 'react-router';
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
 import { useAuth } from '../hooks/auth';
 import PageLoader from '../components/page-loader';
 
 const withAuth = (Component: React.FC) =>
   function withAuthHOC(props: any) {
-    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const OAuthCode = searchParams.get('code');
     const auth = useAuth();
+    const navigate = useNavigate();
     const { isLoggedIn, isLoading } = auth;
     useEffect(() => {
-      if (!isLoggedIn && !isLoading) {
+      if (!isLoggedIn && !isLoading && OAuthCode) {
+        auth.signInWithProvider(OAuthCode, navigate);
+      }
+      if (!isLoggedIn && !isLoading && !OAuthCode) {
         navigate('/sign-in');
       }
-    }, [auth]);
+    }, [isLoggedIn, isLoading, OAuthCode]);
     // eslint-disable-next-line react/jsx-props-no-spreading
     return isLoading ? <PageLoader /> : <Component {...props} />;
   };
