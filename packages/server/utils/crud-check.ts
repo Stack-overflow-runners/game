b4add7afbb858243e68a8df39e65d61960fa1c1c
Aleksandr <loskutov@aleksandrl.ru>
2022-11-27 14:54:31 +0300

    add util for crud check

---
+++ packages/server/utils/crud-check.ts
@@ -0,0 +1,27 @@
+import { User } from '../models';
+
+async function crudCheck() {
+  const C = new User({
+    userId: 1,
+    email: 'test@ya.ru',
+    name: 'name',
+    lastname: 'lastname',
+  });
+  await C.save();
+  const R = await User.findOne({ where: { email: 'test@ya.ru' } });
+  const U = await User.update(
+    { name: 'nameUpdated' },
+    { where: { email: 'test@ya.ru' } }
+  );
+  const D = await User.destroy({ where: { name: 'nameUpdated' } });
+  const CRUD = C && R && U && D;
+
+  if (CRUD) {
+    console.log('CRUD operations success');
+    return true;
+  }
+  console.log('CRUD operations failed');
+  return false;
+}
+
+export default crudCheck;
