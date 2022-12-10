3579a8a2d968b47fd351b2a8e0cf0a20574b8706
Aleksandr <loskutov@aleksandrl.ru>
2022-12-11 02:19:23 +0300

    Add base forum api class

--- packages/client/src/api/base.ts
+++ packages/client/src/api/base.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line max-classes-per-file
 import HttpService from '../utils/http-service';
 import CONSTS from '../utils/consts';
 
@@ -6,5 +7,12 @@ export default class BaseAPI {
 
   constructor(endPoint: string) {
     this.httpService = new HttpService(`${CONSTS.API_URL}${endPoint}`);
+  }
+}
+export class ForumBaseAPI {
+  httpService: HttpService;
+
+  constructor(endPoint: string) {
+    this.httpService = new HttpService(`${CONSTS.FORUM_API_URL}${endPoint}`);
   }
 }
