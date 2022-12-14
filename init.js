47de851e7bb953cf5fea454cb65614e2eec7692a
Aleksandr <loskutov@aleksandrl.ru>
2022-12-14 20:55:55 +0300

    Minor fixes

--- init.js
+++ init.js
@@ -4,5 +4,7 @@ if (!fs.existsSync('.env')) {
   console.log('No .env file found. Copying .env.example to .env');
   fs.copyFileSync('.env.sample', '.env');
 }
-
-fs.mkdirSync('tmp/pgdata', { recursive: true });
+if (!fs.existsSync('tmp')) {
+  console.log('No tmp folder found. Creating tmp folder');
+  fs.mkdirSync('tmp/pgdata', { recursive: true });
+}
